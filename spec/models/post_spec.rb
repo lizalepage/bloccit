efe300c5dd374c6acce80a9a799aeb4676bf64f5
EC2 Default User <ec2-user@ip-172-31-4-82.ec2.internal>
2018-04-21 16:25:28 +0000

    q and a added

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:post)  { Post.create!(title: "New Post Title", body: "New Post Body") }
+    
+    describe "attributes" do
+        it "has title and body attributes" do
+            expect(post).to have_attributes(title: "New Post Title", bosy: "New Post Body")
+        end
+    end    
 end
