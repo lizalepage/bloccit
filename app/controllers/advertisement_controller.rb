3c0c9bea0cc05d245b438b61795e1a8a2ba036d9
EC2 Default User <ec2-user@ip-172-31-4-82.ec2.internal>
2018-04-25 00:16:38 +0000

    advertisement in

---
+++ app/controllers/advertisement_controller.rb
@@ -0,0 +1,32 @@
+class AdvertisementController < ApplicationController
+  def index
+    @advertisement = Advertisement.all
+  end
+
+  def show
+   @advertisement = Advertisement.find(params[:id])
+  end
+
+  def new
+    @advertisement = Advertisement.new
+  end
+
+  def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.copy = params[:advertisement][:copy]
+    @advertisement.price = params[:advertisement][:price]
+
+ 
+     if @advertisement.save
+       flash[:notice] = "Ad was saved."
+       redirect_to @advertisement
+     
+     else
+       flash.now[:alert] = "There was an error saving the Ad. Please try again."
+       render :new
+     end
+  end
+   
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :posts
+
+  resources :posts 
+  
+  resources :advertisement
 
   get 'about' => 'welcome#about'
   
