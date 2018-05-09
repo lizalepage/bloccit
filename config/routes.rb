1ddc764f3264b113405c49fcb7e50928ecbbd4ee
EC2 Default User <ec2-user@ip-172-31-4-82.ec2.internal>
2018-05-09 22:06:47 +0000

    sessions checkpoint done

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   post 'users/confirm' => 'users#confirm'
   
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :topics do
       resources :posts, except: [:index]
