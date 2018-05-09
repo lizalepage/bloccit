0c1e5909d758e8235e447b22e6e29360ace3baac
EC2 Default User <ec2-user@ip-172-31-4-82.ec2.internal>
2018-05-09 22:17:45 +0000

    gravatar added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
+  def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
