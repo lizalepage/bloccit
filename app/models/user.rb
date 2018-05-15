32ce6713b94c036b8b4701e615ae9170df9d8eee
EC2 Default User <ec2-user@ip-172-31-4-82.ec2.internal>
2018-05-15 19:16:55 +0000

    half-way through cp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :votes, dependent: :destroy
     before_save { self.email = email.downcase if email.present?}
 
     before_save :format_user_name
@@ -29,5 +31,5 @@ class User < ApplicationRecord
         end
     end 
     
-    enum role: [:member, :admin]
+    enum role: [:member, :admin, :moderator]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180512183743) do
+ActiveRecord::Schema.define(version: 20180515010544) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -33,7 +33,9 @@ ActiveRecord::Schema.define(version: 20180512183743) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -82,6 +84,16 @@ ActiveRecord::Schema.define(version: 20180512183743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "value"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
