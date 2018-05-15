19818fe2bb8e1c5084bfea26c66d21ed4a4df05a
EC2 Default User <ec2-user@ip-172-31-4-82.ec2.internal>
2018-05-15 20:31:41 +0000

    half way

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,18 @@ class Post < ApplicationRecord
     validates :body, length: { minimum: 20 }, presence: true
     validates :topic, presence: true
     validates :user, presence: true
+    
+    def up_votes
+        votes.where(value: 1).count
+    end
+    
+    def down_votes
+        votes.where(value: -1).count
+        
+    
+    end
+    
+    def points
+        votes.sum(:value)
+    end    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,13 +24,18 @@ topics = Topic.all
 
 50.times do
 
-    Post.create!(
+    post= Post.create!(
         user: users.sample,
         topic:  topics.sample,
         title:  RandomData.random_sentence,
         body:   RandomData.random_paragraph
    )
+   
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
 end
+
 
 posts = Post.all
 
@@ -96,3 +101,4 @@ puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{Advertisement.count} advertisement created"
 puts "#{SponsoredPost.count} sponsored_posts created"
+puts "#{Vote.count} votes created"
