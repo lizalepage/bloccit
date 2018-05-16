79d86c7521b30cb241712edbbad310ea60b6bebf
EC2 Default User <ec2-user@ip-172-31-4-82.ec2.internal>
2018-05-16 00:38:24 +0000

    voting-works

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
     
-    default_scope {order('created_at DESC') }
+    default_scope { order("rank DESC") }
     
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
@@ -17,11 +17,16 @@ class Post < ApplicationRecord
     
     def down_votes
         votes.where(value: -1).count
-        
     
     end
     
     def points
         votes.sum(:value)
+    end 
+    
+    def update_rank
+        age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+        new_rank = points + age_in_days
+        update_attribute(:rank, new_rank)
     end    
 end
